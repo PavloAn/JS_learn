4449eb46040b90bdc5eb48b92a905181fb3cb868
PavloAn <pavloan1@gmail.com>
2024-09-19 16:03:57 +0300

    1 comit

---
+++ l1.js
@@ -0,0 +1,131 @@
+// let name = "Pavlo";
+// let age = 21;
+// let hasWife = false;
+
+// alert(name);
+// document.write(age)
+// console.log(hasWife)
+
+// document.write("<hr>")
+
+
+// let isAdult = confirm("Are u 18?");
+// document.write(isAdult);
+
+// let userName = prompt("What is you name?");
+
+// document.write("<br>")
+// document.write(userName)
+
+
+
+// console.log(age + 1)
+// console.log(age - 4)
+
+// console.log(age % 8)
+
+// console.log(2 > 3)
+// console.log(4 > 3)
+// console.log(10 >= 10)
+
+
+// console.log(7 != "7") 
+// console.log(7 == "7")
+// console.log("----------------")
+// console.log(7 !== "7") 
+// console.log(7 === "7")  
+
+
+// let age = +prompt('enter age');
+
+// if (age >= 18) {
+//   document.write('<h1>Welcome!</h1>')
+// } else {
+//     document.write('<h1>Go home!</h1>')
+// }
+
+
+// let age = +prompt('enter age');
+// let login = prompt('enter login');
+
+// if (age >= 18) {
+//     document.write('<h1>Welcome!</h1>')
+//   } else {
+//     if (login === 'admin') {
+//         document.write('<h1>Welcome!</h1>')
+//     } else {
+//         document.write('<h1>Go home!</h1>')
+//     }
+//   }
+
+
+// let age = +prompt('enter age');
+// let login = prompt('enter login');
+
+// if (age >= 18) {
+//     document.write('<h1>Welcome!</h1>')
+//   } else if (login === 'admin') {
+//     document.write('<h1>Welcome!</h1>')
+//   } else{
+//     document.write('<h1>Go home!</h1>')
+//   }
+
+
+  // || => або
+  // &&=> і
+
+
+// let age = +prompt('enter age');
+// let login = prompt('enter login');
+
+// if (age >= 18 && login === 'admin') {
+//   document.write('<h1>Welcome!</h1>')
+// } else {
+//   document.write('<h1>Go home!</h1>')
+// }
+
+
+// let login = prompt('Enter login'); 
+
+// switch (login) {
+//   case 'admin':
+//     document.write('<h1>Welcome!</h1>')
+//   break;
+//   case 'pavlo':
+//   console.log('pavlo are you dont admin!')
+//   break;
+//   default:
+//     alert('No no no you bandit')
+//   break;
+// }
+
+
+// let number = Math.random() * (50- 20) + 20;
+
+// let number2 = Math.floor(3.9) //=3
+// let number3 = Math.ceil(3.9)  //=4
+// let number4 = Math.round(3.5)  //=3/4 як в школі
+
+// console.log(number4);
+
+
+// let number = 5.34434;
+
+// let s = number.toFixed(4);
+// console.log(+s)  // + означає тип даних числа
+
+
+// let number = '5.34434';  // як перетворити стріншу в число, 3 способи
+
+// console.log(number)
+// console.log(+number)
+// console.log(parseInt(number))
+// console.log(parseFloat(number))
+
+
+// let adm = 'aDMin'; // приводити до великих чи малих
+
+// console.log(adm.toUpperCase());
+// console.log(adm.toLowerCase());
+
+
